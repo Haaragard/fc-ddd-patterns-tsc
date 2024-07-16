1cd6d32f1dde57251f992037b4850b7e2cc8b39e
haaragard <haaragard@gmail.com>
2024-07-15 22:56:28 -0300

    add base domains

---
+++ src/domain/customer/entity/customer.ts
@@ -0,0 +1,57 @@
+import Address from "./valueObject/address";
+
+export default class Customer {
+    
+    _id: string;
+    _name: string;
+    _address: Address;
+    _active: boolean;
+
+    constructor(
+        id: string,
+        name: string,
+        address: Address,
+        active: boolean
+    ) {
+        this._id = id;
+        this._name = name;
+        this._address = address;
+        this._active = active;
+
+        this.validate();
+    }
+
+    validate() {
+        if (this._id.length === 0) {
+            throw new Error("Id is required.");
+        }
+        if (this._name.length === 0) {
+            throw new Error("Name is required.");
+        }
+        if (this._address === undefined) {
+            throw new Error("Address is required.");
+        }
+    }
+
+    get id(): string {
+        return this._id;
+    }
+
+    changeName(name: string) {
+        this._name = name;
+        this.validate();
+    }
+
+    changeAddress(address: Address) {
+        this._address = address;
+        this.validate();
+    }
+
+    activate() {
+        this._active = true;
+    }
+
+    deactivate() {
+        this._active = false;
+    }
+}
---
+++ src/main.ts
@@ -0,0 +1,35 @@
+import Customer from "./domain/customer/entity/customer";
+import Address from "./domain/customer/entity/valueObject/address";
+import OrderItem from "./domain/order/entity/item";
+import Order from "./domain/order/entity/order";
+
+let address = new Address(
+    "Nome Rua",
+    123,
+    "131313123",
+    "City Name"
+);
+let customer = new Customer(
+    "customer-id-123123123",
+    "Customer Name",
+    address,
+    false
+);
+customer.activate();
+
+let item1 = new OrderItem(
+    "order-item-id-1-123123123",
+    "Item Name 1",
+    10
+);
+let item2 = new OrderItem(
+    "order-item-id-2-123123123",
+    "Item Name 2",
+    15
+);
+
+let order = new Order(
+    "order-id-1-123123123",
+    customer.id,
+    [item1, item2]
+);
