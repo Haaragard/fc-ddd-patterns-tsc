b95cb4bc15966339dd7e97066d7c19b04204b6e7
haaragard <haaragard@gmail.com>
2024-07-25 08:34:45 -0300

    add place order method into order service

--- src/domain/order/service/orderService.spec.ts
+++ src/domain/order/service/orderService.spec.ts
@@ -1,8 +1,28 @@
+import CustomerFixture from "../../customer/entity/customerFixture";
 import OrderFixture from "../entity/orderFixture";
 import OrderItemFixture from "../entity/orderItemFixture";
 import OrderService from "./orderService";
 
 describe("Order service unit tests", () => {
+
+    it("Should place an order", () => {
+        let customer = CustomerFixture.create({
+            id: "customer-1",
+            name: "Customer 1",
+        });
+        let orderItem1 = OrderItemFixture.create({
+            id: "order-item-1",
+            productId: "product-1",
+            name: "Order Item 1",
+            price: 10,
+            quantity: 1
+        });
+
+        let order = OrderService.placeOrder(customer, [orderItem1]);
+
+        expect(customer.rewardPoints).toBe(5);
+        expect(order.total()).toBe(10);
+    });
 
     it("Should get total of all orders", () => {
         let orderItem1 = OrderItemFixture.create({
--- src/domain/order/service/orderService.ts
+++ src/domain/order/service/orderService.ts
@@ -1,6 +1,21 @@
+import Customer from "../../customer/entity/customer";
 import Order from "../entity/order";
+import OrderItem from "../entity/orderItem";
+import {v4 as uuid} from "uuid";
 
 export default class OrderService {
+    public static placeOrder(customer: Customer, items: OrderItem[]): Order {
+        if (items.length === 0) {
+            throw new Error("Order must have at least one item.");
+        }
+
+        let order = new Order(uuid(), customer.id, items);
+
+        customer.addRewardPoints(order.total()/2);
+
+        return order;
+    }
+
     public static total(orders: Order[]): number {
         return orders.reduce(
             (acc, order) => acc + order.total(), 0
