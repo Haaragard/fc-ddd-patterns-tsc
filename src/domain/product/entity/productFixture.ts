27d5926ba51b18c7bfc54cc97c7fbca93f78b3c7
haaragard <haaragard@gmail.com>
2024-08-01 23:28:06 -0300

    chore: update product fixture

--- src/domain/product/entity/productFixture.ts
+++ src/domain/product/entity/productFixture.ts
@@ -1,9 +1,10 @@
 import Product from "./product";
+import {v4 as uuid} from "uuid";
 
 export default class ProductFixture {
     public static create(parameters?: Object): Product {
         return new Product(
-            parameters?.hasOwnProperty("id") ? parameters.id : "123",
+            parameters?.hasOwnProperty("id") ? parameters.id : uuid(),
             parameters?.hasOwnProperty("name") ? parameters.name : "Product Name",
             parameters?.hasOwnProperty("price") ? parameters.price : 123,
         );
