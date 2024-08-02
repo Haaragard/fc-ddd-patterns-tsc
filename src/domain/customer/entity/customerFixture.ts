49c29afc8fa8299bffa905168a89f7cdc0f02964
haaragard <haaragard@gmail.com>
2024-08-02 20:00:02 -0300

    feat: update customer fixture

--- src/domain/customer/entity/customerFixture.ts
+++ src/domain/customer/entity/customerFixture.ts
@@ -8,6 +8,17 @@ export default class CustomerFixture {
             ? parameters.address
             : AddressFixture.create();
 
+        let customer = new Customer(
+            parameters?.hasOwnProperty("id") ? parameters.id : uuid(),
+            parameters?.hasOwnProperty("name") ? parameters.name : "Customer name",
+            parameters?.hasOwnProperty("active") ? parameters.active : false,
+            address
+        );
+
+        customer.rewardPoints = parameters?.hasOwnProperty("rewardPoints") ?
+            parameters.rewardPoints :
+            0;
+
         return new Customer(
             parameters?.hasOwnProperty("id") ? parameters.id : uuid(),
             parameters?.hasOwnProperty("name") ? parameters.name : "Customer name",
