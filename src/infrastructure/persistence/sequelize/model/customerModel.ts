db1d4d3285c8452449cc77aba2de73930ca2854c
haaragard <haaragard@gmail.com>
2024-08-02 20:06:24 -0300

    feat: add customer model

---
+++ src/infrastructure/persistence/sequelize/model/customerModel.ts
@@ -0,0 +1,29 @@
+import { Column, Model, PrimaryKey, Table } from "sequelize-typescript";
+
+@Table({ tableName: "customers", timestamps: false })
+export default class CustomerModel extends Model {
+    @PrimaryKey
+    @Column
+    declare id: string;
+
+    @Column
+    declare name: string;
+
+    @Column
+    declare street: string;
+
+    @Column
+    declare number: number;
+    
+    @Column
+    declare zip: string;
+
+    @Column
+    declare city: string;
+
+    @Column
+    declare active: boolean;
+
+    @Column
+    declare rewardPoints: number;
+}
